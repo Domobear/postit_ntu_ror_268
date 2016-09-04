0a4225a3beb3a9a2b438c99bfc1ab4796218b35c
DomobearInNTU <chindarcy@gmail.com>
2016-09-04 01:08:28 -0700

    add show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
 	def show
 		@posts = Post.find(params[:id])
 	end
+
+	def new
+		@posts = Post.new
+	end
+
+	def create
+		post = Post.new(post_params)
+
+		if post.save!
+			redirect_to posts_path
+		else
+			render :new
+		end
+	end
+end
+
+private
+def post_params
+	params.require(:post).permit(:title, :content)
 end
